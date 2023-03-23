2ee5380abb86146124de838e36904896beac218b
Marc13 <83296140+llamaair@users.noreply.github.com>
2023-03-23 12:48:51 +0100

    aa

---
+++ takeoff.js
@@ -0,0 +1,14 @@
+function calculateTakeoffDistance() {
+	const weight = document.getElementById("weight").value;
+	const runwayLength = document.getElementById("runwayLength").value;
+	const windSpeed = document.getElementById("windSpeed").value;
+	const flapSetting = document.getElementById("flapSetting").value;
+
+	// Perform calculations here
+	const takeoffDistance = /* insert calculation formula here */;
+	const takeoffSpeed = /* insert calculation formula here */;
+	const recommendedFlapSetting = /* insert calculation formula here */;
+
+	// Display results
+	document.getElementById("result").innerHTML = `Takeoff Distance Required: ${takeoffDistance} m <br>Takeoff Speed: ${takeoffSpeed} kts <br> Recommended Flap Setting: ${recommendedFlapSetting} %`;
+}
