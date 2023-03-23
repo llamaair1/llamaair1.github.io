bcbe37b04494c1cff6ee5d0ac1a71012f3a5b021
Marc13 <83296140+llamaair@users.noreply.github.com>
2023-03-23 13:30:17 +0100

    added TAF

---
+++ taf.js
@@ -0,0 +1,32 @@
+const form = document.querySelector('form');
+const input = document.querySelector('#icao');
+const taf = document.querySelector('#taf');
+const weather = document.querySelector('#weather');
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  const icao = input.value.trim().toUpperCase();
+  const url = `https://api.checkwx.com/taf/${icao}/decoded`;
+
+  fetch(url, {
+    headers: {
+      'X-API-Key': '2dbd3171e1894be5b82c535013'
+    }
+  })
+  .then(response => response.json())
+  .then(data => {
+    if (data.results === 0) {
+      taf.textContent = 'Invalid ICAO code';
+      weather.textContent = '';
+    } else {
+      const tafText = data.data[0].raw_text;
+      taf.textContent = tafText;
+
+    }
+  })
+  .catch(error => {
+    metar.textContent = 'Error retrieving METAR data';
+    weather.textContent = '';
+    console.error(error);
+  });
+});
