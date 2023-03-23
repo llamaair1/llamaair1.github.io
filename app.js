7d87fbd853eb2235c8a973675509dd4671ef590c
llamaair <83296140+llamaair@users.noreply.github.com>
2023-03-23 11:53:50 +0100

    aa

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const form = document.querySelector('form');
 const input = document.querySelector('#icao');
-const metarRaw = document.querySelector('#metar-raw');
-const metarDecoded = document.querySelector('#metar-decoded');
+const metar = document.querySelector('#metar');
+const weather = document.querySelector('#weather');
 
 form.addEventListener('submit', e => {
   e.preventDefault();
@@ -16,16 +16,51 @@ form.addEventListener('submit', e => {
   .then(response => response.json())
   .then(data => {
     if (data.results === 0) {
-      metarRaw.textContent = 'Invalid ICAO code';
-      metarDecoded.textContent = '';
+      metar.textContent = 'Invalid ICAO code';
+      weather.textContent = '';
     } else {
-      metarRaw.textContent = data.data[0].raw_text;
-      metarDecoded.textContent = JSON.stringify(parseMETAR(data.data[0].raw_text), null, 2);
+      const metarText = data.data[0].raw_text;
+      metar.textContent = metarText;
+
+      // Extract temperature, wind speed, and direction from the METAR data
+      const temperatureMatch = metarText.match(/^(?:METAR|SPECI).+? (M?\d{1,2})\/(M?\d{1,2}) /);
+      const temperatureCelsius = temperatureMatch ? temperatureToCelsius(temperatureMatch[1]) : '';
+      const temperatureFahrenheit = temperatureMatch ? temperatureToFahrenheit(temperatureMatch[1]) : '';
+      const windMatch = metarText.match(/(?:^| )(\d{3}|VRB)(\d{2,3})(?:G(\d{2,3}))?(?:KT|MPS)/);
+      const windDirection = windMatch ? windMatch[1] : '';
+      const windSpeedKnots = windMatch ? parseInt(windMatch[2], 10) : '';
+      const windSpeedMph = windSpeedKnots ? knotsToMph(windSpeedKnots) : '';
+      const windSpeedKph = windSpeedKnots ? knotsToKph(windSpeedKnots) : '';
+      const barometerMatch = metarText.match(/Q(\d{4})/);
+      const barometer = barometerMatch ? `${barometerMatch[1].slice(0, 2)}.${barometerMatch[1].slice(2)} hPa` : 'N/A';
+
+      // Display the temperature, wind speed, and direction in the weather paragraph
+      weather.innerHTML = `Temperature: ${temperatureCelsius}°C / ${temperatureFahrenheit}°F<br>Wind Direction: ${windDirection}<br>Wind Speed: ${windSpeedKnots} knots / ${windSpeedMph} mph / ${windSpeedKph} km/h<br>Barometer: ${barometer} hPA`;
     }
   })
   .catch(error => {
-    metarRaw.textContent = 'Error retrieving METAR data';
-    metarDecoded.textContent = '';
+    metar.textContent = 'Error retrieving METAR data';
+    weather.textContent = '';
     console.error(error);
   });
 });
+
+function temperatureToCelsius(temperature) {
+  const temperatureCelsius = parseInt(temperature, 10) * 0.1;
+  return temperatureCelsius.toFixed(1);
+}
+
+function temperatureToFahrenheit(temperature) {
+  const temperatureFahrenheit = temperatureToCelsius(temperature) * 1.8 + 32;
+  return temperatureFahrenheit.toFixed(1);
+}
+
+function knotsToMph(knots) {
+  const mph = knots * 1.15078;
+  return mph.toFixed(1);
+}
+
+function knotsToKph(knots) {
+  const kph = knots * 1.852;
+  return kph.toFixed(1);
+}
